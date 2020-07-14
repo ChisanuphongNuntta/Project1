62134462af347801b9e24b2a7359c1c9ef50c693
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-14 16:50:17 +0700

    string

--- printstring.py
+++ printstring.py
@@ -1,3 +1,11 @@
 print('Hello world')
 print('Chisanuphong')
-print('Hi')
+print("Hi")
+print('He ask m "Who got the book"')
+print("I'm hungry")
+print("I'am good and \"I will do my best\"")
+print('\ta\na\ta\ta\n\ta')
+print('=========================================')
+print('''\ta
+a\ta\ta
+\ta''')
