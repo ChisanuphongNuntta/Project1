15cb8820b55a4d0131f323555eaecfeeb5680221
INE-02 <INE-02@Admins-iMac-6.local>
2020-08-04 18:06:35 +0700

    Exam

---
+++ EX3Grade.py
@@ -0,0 +1,11 @@
+grade = float(input("Enter your score : "))
+if grade >= 90:
+    print("Grade A")
+elif grade >= 80:
+    print("Grade B")
+elif grade >= 70:
+    print("Grade C")
+elif grade >= 60:
+    print("Grade D")
+elif grade < 60:
+    print("Grade F")
---
+++ Ex4.py
@@ -0,0 +1,18 @@
+print("Please select operation : ")
+print("1.Add \n2.Subtract \n3.Multply \n4.Divide")
+operation = int(input("Select opration from: 1, 2, 3, 4, :1 = "))
+first = int(input("Enter your first number : "))
+second = int(input("Enter your second number  : "))
+if operation == 1 :
+    print(">> ",first," + ", second," = ",(first+second))
+elif operation == 2 :
+    print(">> ",first," - ",second," = ",(first-second) )
+elif operation == 3 :
+    print(">> ",first," x ",second," = ",(first*second))
+elif operation == 4 :
+    print(">> ",first," ÷ ",second," = ",(first/second))
+else:
+    print("!!Error!! Don't have opration")
+ 
+
+
