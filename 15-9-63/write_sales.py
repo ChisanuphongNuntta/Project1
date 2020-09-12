fc72785e3694c1ceceb5ebd6ceda126489ae5b5b
Chisanuphong Nuntta <heart@MacBook-Air-khxng-Chisanuphong.local>
2020-09-13 00:00:17 +0700

    Heart

--- 15-9-63/write_sales.py
+++ 15-9-63/write_sales.py
@@ -2,7 +2,7 @@ def main():
     num_days = int(input('For how many days do ' + \
                             'you have sales? '))
 
-    sales_file = open('sale.txt','w')
+    sales_file = open('sales.txt','w')
 
     for count in range(1,num_days + 1):
         sales = float(input('Enter the sales for day #' + \
