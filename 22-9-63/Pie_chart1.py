d60c14eb67cc7c2f21973fcfcab7191caa5a207e
ChisanuphongNuntta <55497174+ChisanuphongNuntta@users.noreply.github.com>
2020-11-01 16:15:53 +0700

    Heart

--- 22-9-63/Pie_chart1.py
+++ 22-9-63/Pie_chart1.py
@@ -4,7 +4,7 @@ def main():
     sale = [100,400,300,600]
 
     slice_labels = ['1st Qtr','2nd Qtr','3rd Qtr','4th Qtr']
-    plt.pie(sales,labels=slice_labels)
+    plt.pie(sale,labels=slice_labels)
     plt.title('Sales by Quarter')
     plt.show()
 
